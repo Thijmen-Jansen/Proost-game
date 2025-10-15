84a2b20e99ee0b98f4b2866ff8da15fd2d31743e
Thijmen-Jansen <t.jansen24@student.han.nl>
2025-10-15 14:35:21 +0200

    index gemaakt + begin van hostScreen

---
+++ my-app/app/hostScreen.tsx
@@ -0,0 +1,9 @@
+import { View } from "react-native";
+
+export default function HostScreen(){
+    return(
+        <View>
+            
+        </View>
+    )
+}
--- my-app/app/index.tsx
+++ my-app/app/index.tsx
@@ -1,4 +1,7 @@
 import { Text, View } from "react-native";
+import { TextInput } from 'react-native';
+import { Button } from 'react-native';
+import { Switch } from 'react-native';
 
 export default function Index() {
   return (
@@ -9,7 +12,65 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Title></Title>
+      <JoinLobby></JoinLobby>
+      <HostGame></HostGame>
+
     </View>
   );
+}
+
+export function Title() {
+  return (
+    <View
+    style={{
+      margin: 50,
+    }}>
+
+      <Text style={{
+        fontSize: 30,
+        fontWeight: 'bold',
+      }}> Proost🍻</Text>
+
+    </View>
+  )
+}
+
+export function JoinLobby(){
+  return(
+    <View
+    style={{
+      margin: 50
+    }}>
+      <Text
+      style={{
+        fontWeight: 'bold',
+      }}>
+        Join lobby</Text>
+      <View
+      style={{
+        flexDirection: 'row',
+      }}>
+        <TextInput
+          placeholder="Typ hier de code (bv. 662011)"
+          style={{ borderWidth: 1, padding: 8 }}
+        />
+        <Button title="Join" onPress={() => alert('Je hebt geklikt!')} />
+      </View>
+    </View>
+  )
+}
+
+export function HostGame(){
+  return (
+    <View>
+    <Text
+    style={{
+      fontWeight: 'bold',
+      fontSize: 20
+    }}>
+      Host Game</Text>
+    <Button title="Host Game" onPress={() => alert('Je hebt geklikt!')} />
+    </View>
+  )
 }
